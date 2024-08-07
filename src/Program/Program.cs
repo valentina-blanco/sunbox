c4b05db21ed3918f53782822ba550150f36cb5d6
mateocimassi <mateo.cimassi@correo.ucu.edu.uy>
2024-08-07 12:27:30 -0300

    commit

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 Console.WriteLine("hello");
+Console.WriteLine("hello");
