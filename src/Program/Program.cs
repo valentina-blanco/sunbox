acb1f4bc337b68f967b1d1402f4cae29cb012cf9
mateocimassi <mateo.cimassi@correo.ucu.edu.uy>
2024-08-07 12:12:41 -0300

    hago la multiplicacion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine(Addition.Add(7, 2));
+﻿//Agrego un comentario
+Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
