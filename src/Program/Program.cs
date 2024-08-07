734a29d1a9660d63ca3004ceb1526322f5c49703
mateocimassi <mateo.cimassi@correo.ucu.edu.uy>
2024-08-07 12:20:58 -0300

    comentario hello

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+Console.WriteLine("hello");
