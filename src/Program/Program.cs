79e2dc9d8dc9082d115e28451a6ef516adafa5ae
Nicolas Perez <nicolas.pgarcia@correo.ucu.edu.uy>
2024-08-12 10:23:28 -0300

    multiplicacion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -4,3 +4,12 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 Console.WriteLine("Holaaaaaaaaa");
+
+
+public class Multiplication
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
